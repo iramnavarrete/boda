26f64114fdcd6d3ffa3af8887f36030c98bdfda8
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2025-07-14 15:28:12 -0600

    Se cambia de lugar la imagen para no usar la ruta raíz

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,10 +22,10 @@ export default function Document() {
           property="article:modified_time"
           content="2025-07-14T14:48:01-06:00"
         />
-        <meta property="og:image" content="https://bodajy.info/cover.png" />
+        <meta property="og:image" content="https://bodajy.info/img/cover.png" />
         <meta property="og:image:width" content="630" />
         <meta property="og:image:height" content="420" />
-        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:type" content="image/png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:label1" content="Tiempo de lectura" />
         <meta name="twitter:data1" content="3 minutos" />
