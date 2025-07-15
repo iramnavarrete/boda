cb5cdc7617a801027723840cbd187d5a1ec3bc2d
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2025-07-14 22:10:17 -0600

    Cambio de portada en miniatura de facebook y whatsapp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,12 +20,12 @@ export default function Document() {
         <meta property="og:url" content="https://bodajy.info" />
         <meta
           property="article:modified_time"
-          content="2025-07-14T14:48:01-06:00"
+          content="2025-07-14T22:08:01-06:00"
         />
         <meta property="og:image" content="https://bodajy.info/img/cover.png" />
         <meta property="og:image:width" content="630" />
         <meta property="og:image:height" content="420" />
-        <meta property="og:image:type" content="image/png" />
+        {/* <meta property="og:image:type" content="image/png" /> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:label1" content="Tiempo de lectura" />
         <meta name="twitter:data1" content="3 minutos" />
