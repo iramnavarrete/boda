fce4ea75726abca1a920b02747021caf29f097cf
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2025-07-17 15:31:35 -0600

    Se agrega botón de música y animación de entrada para la misma

--- src/pages/sections/music.tsx
+++ src/pages/sections/music.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useRef, useState } from "react";
 function Music() {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [progressWidth, setProgressWidth] = useState("0%");
 
   const toggleAudio = () => {
     if (isPlaying) {
@@ -17,36 +16,15 @@ function Music() {
     }
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const duration = audioRef.current?.duration;
-      const elapsed = audioRef.current?.currentTime;
-      if (duration && elapsed) {
-        const progress = (elapsed / duration) * 100;
-        setProgressWidth(`${progress}%`);
-      }
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
-    <div className="flex bg-primary w-full p-6">
+    <div className="flex bg-accent p-6 rounded-full absolute bottom-6 right-5 drop-shadow-[0px_2px_2px_rgba(0,0,0,0.25)]">
       <div className="flex flex-row gap-4 w-full">
         <div onClick={toggleAudio}>
           {isPlaying ? (
-            <PauseIcon className="w-7 h-7 text-white" />
+            <PauseIcon className="w-7 h-7 text-primary" />
           ) : (
-            <PlayIcon className="w-7 h-7 text-white" />
+            <PlayIcon className="w-7 h-7 text-primary" />
           )}
-        </div>
-        <div className="flex w-full items-center relative">
-          <hr className="w-full border-[#b2b2b2] border-2" />
-          <hr
-            className="absolute border-white border-2 left-0 z-10"
-            style={{ width: progressWidth }}
-          />
         </div>
       </div>
       <audio ref={audioRef} loop src="/music.mp3" />
