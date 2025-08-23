f850c99162b15cf0def63c9dc9fb0655ef346097
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2025-08-23 12:07:57 -0600

    Se agrega background tipo lino en los excedentes de la invitación

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -166,7 +166,7 @@ export default function Home() {
       </div>
 
       <div style={{ overflow: "hidden" }}>
-        <div className="flex flex-col items-center bg-accent overflow-hidden">
+        <div className="flex flex-col items-center overflow-hidden bg-texture">
           <div className="hidden sm:block fixed left-0 top-0 bottom-0 w-[calc(50%-250px)] 2xl:w-[calc(50%-300px)] opacity-60 place-content-center text-center">
             <FlowersCoverUp className="w-full text-primary drop-shadow-none hidden 2xl:block" />
             <p className="flex items-center justify-center font-newIconScript text-primary text-4xl 2xl:text-4xl -rotate-90 2xl:-rotate-0 2xl:py-12">
@@ -187,7 +187,7 @@ export default function Home() {
             <Footer />
             <AudioController />
           </div>
-          <div className="hidden sm:block fixed right-0 top-0 bottom-0 w-[calc(50%-250px)] 2xl:w-[calc(50%-300px)] opacity-60 place-content-center text-center">
+          <div className="hidden sm:block fixed right-0 top-0 bottom-0 w-[calc(50%-250px)] 2xl:w-[calc(50%-300px)] opacity-60 place-content-center text-center bg-texture">
             <FlowersCoverUp className="w-full text-primary drop-shadow-none hidden 2xl:block scale-x-[-1]" />
             <p className="flex gap-2 items-center justify-center font-newIconScript text-primary text-4xl 2xl:text-4xl rotate-90 2xl:rotate-0 2xl:py-12">
               20 <span>/</span> 10 <span>/</span> 25
