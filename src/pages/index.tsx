9ce5b86453a8b5af5f2e6e23005c1716aadbb1fc
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2025-09-04 22:57:31 -0600

    Se actualiza fecha del excedente, era 20 en vez de 25

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -190,7 +190,7 @@ export default function Home() {
           <div className="hidden sm:block fixed right-0 top-0 bottom-0 w-[calc(50%-250px)] 2xl:w-[calc(50%-300px)] opacity-60 place-content-center text-center bg-texture">
             <FlowersCoverUp className="w-full text-primary drop-shadow-none hidden 2xl:block scale-x-[-1]" />
             <p className="flex gap-2 items-center justify-center font-newIconScript text-primary text-4xl 2xl:text-4xl rotate-90 2xl:rotate-0 2xl:py-12">
-              20 <span>/</span> 10 <span>/</span> 25
+              25 <span>/</span> 10 <span>/</span> 25
             </p>
             <FlowersCoverDown className="w-full drop-shadow-none text-primary hidden 2xl:block scale-x-[-1]" />
           </div>
