63fe40f29e77fb0fd89bbf0a64ba9f129f23fa29
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2026-01-10 12:31:16 -0600

    Se elimina bg del Loader para que tenga fondo transparente siempre

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -9,7 +9,7 @@ const Loader: FC<Props> = ({ fullscreen = false }) => {
     <div
       className={`${
         fullscreen ? "min-h-screen" : ""
-      } bg-stone-50 flex items-center justify-center`}
+      } flex items-center justify-center`}
     >
       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-yellow-600"></div>
     </div>
