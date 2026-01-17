1b4c1729e8856cdc73fa7fe983eca3c01937927d
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2026-01-16 19:32:52 -0600

    Se agrega un HOC para controlar que todas las páginas que estén dentro de admin, tengan un usuario con la sesión iniciada, de lo contrario los enviará al login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import { AuthInitializer } from "@/features/admin/components/AuthInitializer";
+import ProtectedPage from "@/features/admin/components/ProtectedPage";
 import { ToastProvider } from "@/features/shared/components/Toast";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isAdminRoute = router.pathname.startsWith("/admin");
+
   return (
     <ToastProvider>
       <AuthInitializer />
-      <Component {...pageProps} />
+      {isAdminRoute ? (
+        <ProtectedPage>
+          <Component {...pageProps} />
+        </ProtectedPage>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </ToastProvider>
   );
 }
