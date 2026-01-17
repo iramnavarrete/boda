20e0373f079990f9a2e3391aed2f60dc30bee7cb
Iram Navarrete <iram.navarrete@plumanegra.com.mx>
2026-01-16 19:38:35 -0600

    Se integra ruta de AdminDashboard

---
+++ src/pages/admin/invitations/[invitationId]/dashboard/index.tsx
@@ -0,0 +1,19 @@
+import { InvitationDashboard } from "@/features/admin/components/InvitationDashboard";
+import Loader from "@/features/front/components/Loader";
+import { useRouter } from "next/router";
+
+export default function AdminDashboard() {
+  const router = useRouter();
+  const invitationId = router.query.invitationId as string;
+
+  if (!invitationId) {
+    return <Loader fullscreen />;
+  }
+  return (
+    <InvitationDashboard
+      invitationId={invitationId}
+      onBack={() => {}}
+      onGoToList={() => {}}
+    />
+  );
+}
