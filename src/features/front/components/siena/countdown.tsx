e05160d9744d4499d115e347f285efb6118d1021
Iram Navarrete <32463351+iramnavarrete@users.noreply.github.com>
2026-08-02 01:01:05 -0600

    Actualizar countdown.tsx

--- src/features/front/components/siena/countdown.tsx
+++ src/features/front/components/siena/countdown.tsx
@@ -89,7 +89,7 @@ export default function CountDown({
           className="absolute inset-0 w-full h-full z-0"
           style={{ clipPath: "inset(0 0 0 0)" }}
         >
-          <div className="fixed top-0 max-w-[500px] 2xl:max-w-[600px] w-full h-screen transform-gpu">
+          <div className="fixed top-0 max-w-[500px] 2xl:max-w-[600px] w-full h-screen">
             <Image
               ref={imageRef}
               src={backgroundImage}
