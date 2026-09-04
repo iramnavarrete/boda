5341cfaafbff94f79b467bc2f1e58670bbe3a9ba
iram_navarrete <iram.navarrete@plumanegra.com.mx>
2026-09-04 17:36:43 -0600

    Se agrega icons como candidato para clases de tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/icons/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
