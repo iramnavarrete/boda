1906431a19614a666fbe8c395ff41491ff89cb12
iram_navarrete <iram.navarrete@plumanegra.com.mx>
2026-09-07 14:53:11 -0600

    Migración de tipografias a un formato de comprensión más optimo para web, se reduce en un 60% el tamaño de las tipografias

--- src/features/shared/fonts/index.ts
+++ src/features/shared/fonts/index.ts
@@ -3,91 +3,91 @@
 import localFont from "next/font/local";
 
 export const newIconScript = localFont({
-  src: "../../../../src/fonts/New-Icon-Script.otf",
+  src: "../../../../src/fonts/NewIconScript-Regular.woff2",
   variable: "--font-new-icon-script",
   display: "swap", // Mejora la carga dinámica (evita bloqueos de texto)
 });
 
 export const nourdLight = localFont({
-  src: "../../../../src/fonts/nourd_light.ttf",
+  src: "../../../../src/fonts/Nourd-Light.woff2",
   variable: "--font-nourd-light",
   display: "swap",
 });
 
 export const nourdMedium = localFont({
-  src: "../../../../src/fonts/nourd_medium.ttf",
+  src: "../../../../src/fonts/Nourd-Medium.woff2",
   variable: "--font-nourd-medium",
   display: "swap",
 });
 
 export const nourdBold = localFont({
-  src: "../../../../src/fonts/nourd_bold.ttf",
+  src: "../../../../src/fonts/Nourd-Bold.woff2",
   variable: "--font-nourd-bold",
   display: "swap",
 });
 
 export const autography = localFont({
-  src: "../../../../src/fonts/autography.otf",
+  src: "../../../../src/fonts/Autography.woff2",
   variable: "--font-autography",
   display: "swap",
 });
 
 export const comprehensionDark = localFont({
-  src: "../../../../src/fonts/comprehension_dark.otf",
+  src: "../../../../src/fonts/Comprehension-Dark.woff2",
   variable: "--font-comprehension-dark",
   display: "swap",
 });
 
 export const comprehensionSemiBold = localFont({
-  src: "../../../../src/fonts/comprehension_semibold.otf",
+  src: "../../../../src/fonts/Comprehension-SemiBold.woff2",
   variable: "--font-comprehension-semi-bold",
   display: "swap",
 });
 
 export const aboveBeyondScript = localFont({
-  src: "../../../../src/fonts/above_beyond_script.otf",
+  src: "../../../../src/fonts/AbovetheBeyond-Script.woff2",
   variable: "--font-above-beyond-script",
   display: "swap",
 });
 
 export const rhymeFormal = localFont({
-  src: "../../../../src/fonts/rhyme-formal.otf",
+  src: "../../../../src/fonts/Rhyme-Formal.woff2",
   variable: "--font-rhyme-formal",
   display: "swap",
 });
 
 export const edwardianScriptItc = localFont({
-  src: "../../../../src/fonts/edwardian-script-itc.ttf",
+  src: "../../../../src/fonts/EdwardianScriptITC.woff2",
   variable: "--font-edwardian-script-itc",
   display: "swap",
 });
 
 export const greatVibes = localFont({
-  src: "../../../../src/fonts/great-vibes.ttf",
+  src: "../../../../src/fonts/GreatVibes-Regular.woff2",
   variable: "--font-great-vibes",
   display: "swap",
 });
 
 export const gistesy = localFont({
-  src: "../../../../src/fonts/gistesy.ttf",
+  src: "../../../../src/fonts/Gistesy.woff2",
   variable: "--font-gistesy",
   display: "swap",
 });
 
 export const alexBrush = localFont({
-  src: "../../../../src/fonts/alex-brush.ttf",
+  src: "../../../../src/fonts/AlexBrush-Regular.woff2",
   variable: "--font-alex-brush",
   display: "swap",
 });
 
 export const ebGaramondItalic = localFont({
-  src: "../../../../src/fonts/eb-garamond-italic.ttf",
+  src: "../../../../src/fonts/EBGaramond-Italic.woff2",
   variable: "--font-eb-garamond-italic",
   display: "swap",
 });
 
 export const pinyonScript = localFont({
-  src: "../../../../src/fonts/pinyon-script.ttf",
+  src: "../../../../src/fonts/PinyonScript-Regular.woff2",
   variable: "--font-pinyon-script",
   display: "swap",
 });
